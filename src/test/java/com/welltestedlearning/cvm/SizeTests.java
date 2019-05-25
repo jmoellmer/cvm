cd48ba7acb0bcca86b6375049f257d22baf2d5a2
Jeffery A. Moellmer <jeff.moellmer@gmail.com>
2019-05-24 19:35:44 -0500

    Tested size class directly.

--- src/test/java/com/welltestedlearning/cvm/SizeTests.java
+++ src/test/java/com/welltestedlearning/cvm/SizeTests.java
@@ -7,91 +7,58 @@ public class SizeTests {
 
     @Test
     public void smallCoffeeIs100() throws Exception {
-        CoffeeOrder coffeeOrder = new CoffeeOrder();
+        Size size = new Size("small");
 
-        coffeeOrder.addCoffee("small");
-
-        assertThat(coffeeOrder.price())
+        assertThat(size.price())
                 .isEqualTo(100);
     }
     
     @Test
     public void mediumCoffeeIs150() throws Exception {
-        CoffeeOrder coffeeOrder = new CoffeeOrder();
+        Size size = new Size("medium");
 
-        coffeeOrder.addCoffee("medium");
-
-        assertThat(coffeeOrder.price())
+        assertThat(size.price())
                 .isEqualTo(150);
     }
 
     @Test
     public void largeCoffeeIs200() throws Exception {
-        CoffeeOrder coffeeOrder = new CoffeeOrder();
+        Size size = new Size("large");
 
-        coffeeOrder.addCoffee("large");
-
-        assertThat(coffeeOrder.price())
+        assertThat(size.price())
                 .isEqualTo(200);
     }
 
     @Test
     public void xlCoffeeIs300() throws Exception {
-        CoffeeOrder coffeeOrder = new CoffeeOrder();
+        Size size = new Size("xl");
 
-        coffeeOrder.addCoffee("xl");
-
-        assertThat(coffeeOrder.price())
+        assertThat(size.price())
                 .isEqualTo(300);
     }
 
     @Test
     public void emptyCoffeeIs0() throws Exception {
-        CoffeeOrder coffeeOrder = new CoffeeOrder();
+        Size size = new Size("");
 
-        coffeeOrder.addCoffee("");
-
-        assertThat(coffeeOrder.price())
+        assertThat(size.price())
                 .isZero();
     }
 
     @Test
     public void smallDisplayIsSizeSmall() throws Exception {
-        CoffeeOrder coffeeOrder = new CoffeeOrder();
+        Size size = new Size("small");
 
-        coffeeOrder.addCoffee("small");
-
-        assertThat(coffeeOrder.display())
-                .isEqualTo("Size: Small\nPrice: $1.00\n");
+        assertThat(size.display())
+                .isEqualTo("Size: Small");
     }
 
-    @Test
-    public void mediumDisplayIsSizeMedium() throws Exception {
-        CoffeeOrder coffeeOrder = new CoffeeOrder();
-
-        coffeeOrder.addCoffee("medium");
-
-        assertThat(coffeeOrder.display())
-                .isEqualTo("Size: Medium\nPrice: $1.50\n");
-    }
-
-    @Test
-    public void  largeDisplayIsSizeLarge() throws Exception {
-        CoffeeOrder coffeeOrder = new CoffeeOrder();
-
-        coffeeOrder.addCoffee("large");
-
-        assertThat(coffeeOrder.display())
-                .isEqualTo("Size: Large\nPrice: $2.00\n");
-    }
     @Test
     public void xlDisplayIsSizeXL() throws Exception {
-        CoffeeOrder coffeeOrder = new CoffeeOrder();
+        Size size = new Size("xl");
 
-        coffeeOrder.addCoffee("xl");
-
-        assertThat(coffeeOrder.display())
-                .isEqualTo("Size: XL\nPrice: $3.00\n");
+        assertThat(size.display())
+                .isEqualTo("Size: XL");
     }
 
 }
