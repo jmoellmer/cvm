b815a88215be7f7ef4582010ed70e4cb80cc1fb9
Jeff Moellmer <jeff.moellmer@gmail.com>
2019-10-08 21:24:47 -0500

    Added toString() to enum class

--- src/main/java/com/welltestedlearning/cvm/SizeOption.java
+++ src/main/java/com/welltestedlearning/cvm/SizeOption.java
@@ -2,4 +2,10 @@ package com.welltestedlearning.cvm;
 
 public enum SizeOption {
     SMALL, MEDIUM, LARGE;
+
+    @Override
+    public String toString() {
+        String name = name().toString();
+        return Character.toUpperCase(name.charAt(0)) + name.substring(1);
+    }
 }
