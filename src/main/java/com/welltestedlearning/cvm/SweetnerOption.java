d19d51e55151d6f731fa30eacc3034a151d1be9f
Jeff Moellmer <jeff.moellmer@gmail.com>
2019-10-24 10:48:24 -0500

    Implemented SweetnerOption

--- src/main/java/com/welltestedlearning/cvm/SweetnerOption.java
+++ src/main/java/com/welltestedlearning/cvm/SweetnerOption.java
@@ -5,6 +5,9 @@ public enum SweetnerOption {
 
     @Override
     public String toString() {
-        return "SweetnerOption{}";
+        String name = name().toString();
+        StringBuilder properName = new StringBuilder();
+        properName.append(Character.toUpperCase(name.charAt(0)));
+        return properName.append(name.substring(1).toLowerCase()).toString();
     }
 }
